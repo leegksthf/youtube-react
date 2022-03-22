9a0e4872b214146b9a46d4209f39c1b744bbc172
suu <9l0522@naver.com>
2022-03-22 18:10:51 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-        <h1>
-        {props.video.snippet.title}
-        </h1>
+const VideoItem = ({ video: {snippet} }) => (
+    // ({video: videoItem}) : props.video 반복해서 쓰기 싫을 때 일케 써줌. props 안에 동일한 이름이 있다면 props 안에 있는 것이 바로 여기에 할당됨.
+    // ({video: {snippet}}) : video 안에 있는 snippet을 받음. 바로 snippet부터 적어주면 됨.
+    <li className={styles.container}>
+        <div className={styles.video}>
+        <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="" />    
+            <div className={styles.metadata}>
+            <p className={styles.title}>{snippet.title}</p>
+            <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div>
+            </div>
+    </li>
     );
 
 export default VideoItem;
