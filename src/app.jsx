6c698632458d2a3cadc44292147ee47f51481425
suu <9l0522@naver.com>
2022-03-22 19:20:55 +0900

    add search

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCQo2FrlCa3nK0-adJ6S5zelNpXrW_MoZw`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId }))
+      )
+    .then(items => setVideos(items))
+    .catch(error => console.log('error', error));
+  };
 
   useEffect(() => {
     const requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+      method: 'GET',
+      redirect: 'follow'
+    };
 
-fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCQo2FrlCa3nK0-adJ6S5zelNpXrW_MoZw", requestOptions)
-  .then(response => response.json())
-  .then(result => setVideos(result.items))
-  .catch(error => console.log('error', error));
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCQo2FrlCa3nK0-adJ6S5zelNpXrW_MoZw", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
