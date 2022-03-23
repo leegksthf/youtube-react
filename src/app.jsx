4db5bb570446c5ee68aa89da55fd863525da500d
suu <9l0522@naver.com>
2022-03-23 16:49:04 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,33 +3,18 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {   // 여기 안에서 const youtube = new Youtube(); 해주면 계속적으로 호출되기때문에 app을 사용하는 최종위치(index)인 외부에서 받아와야함.
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCQo2FrlCa3nK0-adJ6S5zelNpXrW_MoZw`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId }))
-      )
-    .then(items => setVideos(items))
-    .catch(error => console.log('error', error));
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCQo2FrlCa3nK0-adJ6S5zelNpXrW_MoZw", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
